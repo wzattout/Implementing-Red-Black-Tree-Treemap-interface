afb8316d5f9c72a0f7abf0d5658504eb7f39af31
ziadabdelkader <zyad012ziad012@gmail.com>
2020-04-09 03:21:57 +0200

    implementing node

--- src/eg/edu/alexu/csd/filestructure/redblacktree/Node.java
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/Node.java
@@ -1,69 +1,81 @@
 package eg.edu.alexu.csd.filestructure.redblacktree;
 
 public class Node<T extends Comparable<T>, V> implements INode<T, V> {
-
+    INode<T, V> parent;
+    INode<T, V> leftChild;
+    INode<T, V> rightChild;
+    T key;
+    V value;
+    boolean color;
+    public Node(T key,V value,INode<T, V> parent){
+        this.key=key;
+        this.value=value;
+        this.parent=parent;
+        //node is created with default color red
+        this.color=true;
+    }
     @Override
     public void setParent(INode<T, V> parent) {
-
+        this.parent=parent;
     }
 
     @Override
     public INode<T, V> getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public void setLeftChild(INode<T, V> leftChild) {
-
+        this.leftChild=leftChild;
     }
 
     @Override
     public INode<T, V> getLeftChild() {
-        return null;
+        return this.leftChild;
     }
 
     @Override
     public void setRightChild(INode<T, V> rightChild) {
-
+        this.rightChild=rightChild;
     }
 
     @Override
     public INode<T, V> getRightChild() {
-        return null;
+        return this.rightChild;
     }
 
     @Override
     public T getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
     public void setKey(T key) {
-
+        this.key=key;
     }
 
     @Override
     public V getValue() {
-        return null;
+        return this.value;
     }
 
     @Override
     public void setValue(V value) {
-
+        this.value=value;
     }
 
     @Override
     public boolean getColor() {
-        return false;
+        return this.color;
     }
 
     @Override
     public void setColor(boolean color) {
-
+        this.color=color;
     }
 
     @Override
     public boolean isNull() {
-        return false;
+        return this.value==null;
     }
 }
